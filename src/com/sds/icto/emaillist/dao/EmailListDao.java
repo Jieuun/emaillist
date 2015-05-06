5d70a49738d2635b7837aa6c9b8103a1c839513c
Jieun/icto55-2 <lee920328@naver.com>
2015-05-06 17:30:06 +0900

    emaillist

---
+++ src/com/sds/icto/emaillist/dao/EmailListDao.java
@@ -0,0 +1,16 @@
+
+package com.sds.icto.emaillist.dao;
+
+import java.util.*;
+import com.sds.icto.emaillist.vo.EmailListVo;
+
+public class EmailListDao {
+	public void insert(){
+		
+	}
+	
+	public List<EmailListVo> fetchlist(){
+		return null;
+		
+	}
+}
